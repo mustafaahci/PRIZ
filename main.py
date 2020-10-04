5a4b4748b20d1a4e46a36af64f90b145a02c8b3d
Mustafa AHCI <mustafapassahci@gmail.com>
2020-10-05 00:05:23 +0300

    Splash Screen & Program List Widget etc.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-import ctypes
-import json
 import os
 import sys
+import json
 import typing
+import ctypes
 from ctypes import wintypes
+from time import time, sleep
 
-import PySide2
+from PySide2.QtCore import QRect, Qt, QSize, QFileInfo
+from PySide2.QtGui import QPixmap, QPalette
+from PySide2.QtWidgets import QLayout, QFrame, QVBoxLayout, QWidget, QApplication, QLineEdit, QLabel, QHBoxLayout, \
+    QListWidget, QListWidgetItem, QFileIconProvider, QFileSystemModel, QSplashScreen
+
 import win32api
 import win32gui
-from PySide2.QtCore import QRect, Qt
-from PySide2.QtGui import QPixmap
-from PySide2.QtWidgets import QLayout, QFrame, QVBoxLayout, QWidget, QApplication, QLineEdit, QLabel, QHBoxLayout
+import win32com.client
 from PySide2.QtWinExtras import QtWin
 from win32con import GWL_STYLE, WM_NCCALCSIZE, WS_CAPTION, HWND_TOPMOST, SWP_NOMOVE, SWP_NOSIZE, SWP_SHOWWINDOW
 
@@ -21,9 +24,27 @@ def readCss(path) -> str:
     return style
 
 
-def clearLayout(layout: QLayout) -> None:
+def clearLayout(layout: QLayout) -> QLayout:
     layout.setContentsMargins(0, 0, 0, 0)
     layout.setSpacing(0)
+    return layout
+
+
+class SplashScreen(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setFixedSize(1040, 140)
+        self.setAttribute(Qt.WA_DeleteOnClose)
+        win32gui.SetWindowPos(self.winId(), HWND_TOPMOST, 0, 0, 0, 0,
+                              SWP_NOMOVE | SWP_NOSIZE | SWP_SHOWWINDOW)
+
+        __background = QPixmap("image/SplashScreen.png")
+        __background.scaled(self.size())
+        __palette = QPalette()
+        __palette.setBrush(QPalette.Background, __background)
+        self.setPalette(__palette)
 
 
 class ListWidget(QWidget):
@@ -70,20 +91,57 @@ class MainFrame(QFrame):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setObjectName("MainFrame")
+        self.shell = win32com.client.Dispatch("WScript.Shell")
+        self.provider = QFileIconProvider()
 
         _layout = QVBoxLayout()
-        _layout.setSpacing(0)
-        _layout.setContentsMargins(20, 10, 20, 10)
+        _layout.setSpacing(20)
+        _layout.setContentsMargins(20, 20, 20, 20)
 
         self.entry = QLineEdit()
         self.entry.setPlaceholderText("Please type 'exit' to exit!")
         self.entry.setObjectName("entry")
 
+        self.result_list = QListWidget()
+        self.result_list.setObjectName("result_list")
+        self.result_list.itemDoubleClicked.connect(self.openProgram)
+
         _layout.addWidget(self.entry)
-
+        _layout.addWidget(self.result_list)
         self.setStyleSheet(readCss("style/main.css"))
-
         self.setLayout(_layout)
+        self.getApps()
+
+    def openProgram(self):
+        os.startfile(self.result_list.currentItem().text())
+
+    @staticmethod
+    def getAppIcon(path):
+        fin = QFileInfo(path)
+        model = QFileSystemModel()
+
+        model.setRootPath(fin.path())
+        qq = model.iconProvider()
+        icon = qq.icon(fin)
+        return icon
+
+    def getApps(self):
+        for app_path in PACKAGE["APPS"]:
+            app_name = str(os.path.basename(app_path)).split(".")[0]
+            print(app_path)
+
+            new_widget = ListWidget(app_name)
+            new_widget.setText(app_name, app_path)
+
+            icon = self.getAppIcon(app_path)
+
+            pixmap = icon.pixmap(icon.actualSize(QSize(32, 32)))
+            new_widget.setIcon(pixmap)
+
+            new_item = QListWidgetItem(app_path, self.result_list)
+            new_item.setSizeHint(new_widget.sizeHint())
+            self.result_list.addItem(new_item)
+            self.result_list.setItemWidget(new_item, new_widget)
 
 
 class MainWindow(QWidget):
@@ -91,10 +149,19 @@ class MainWindow(QWidget):
         super().__init__(parent)
 
         self.setObjectName("MainWindow")
-        self.setWindowFlags(Qt.FramelessWindowHint
-                            | Qt.WindowMinimizeButtonHint)
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowMinimizeButtonHint)
 
         self.__press_pos = None
+
+        _layout = clearLayout(QVBoxLayout())
+        self._main_frame = MainFrame(self)
+        _layout.addWidget(self._main_frame)
+
+        # self.frame = QFrame(self)
+        # self.frame.setStyleSheet("background: green;")
+        # self.frame.setGeometry(0, 95, 250, 5)
+
+        self.show()
 
         hWnd = self.winId()
         style = win32gui.GetWindowLong(hWnd, GWL_STYLE)
@@ -108,17 +175,7 @@ class MainWindow(QWidget):
         else:
             QtWin.resetExtendedFrame(self)
 
-        _layout = QVBoxLayout()
-        clearLayout(_layout)
-
         _rect: QRect = QApplication.instance().desktop().availableGeometry(self)
-
-        self._main_frame = MainFrame(self)
-        _layout.addWidget(self._main_frame)
-
-        # self.frame = QFrame(self)
-        # self.frame.setStyleSheet("background: green;")
-        # self.frame.setGeometry(0, 95, 250, 5)
 
         x = _rect.width() / 2 - WIDTH / 2
         y = _rect.height() / 2 - HEIGHT / 2 - _rect.height() * 15 / 100
@@ -141,7 +198,7 @@ class MainWindow(QWidget):
         if self.__press_pos:
             self.move(self.pos() + (event.pos() - self.__press_pos))
 
-    def nativeEvent(self, eventType: PySide2.QtCore.QByteArray, message: int) -> typing.Tuple:
+    def nativeEvent(self, eventType, message: int) -> typing.Tuple:
         retval, result = super().nativeEvent(eventType, message)
 
         if eventType == "windows_generic_MSG":
@@ -168,12 +225,14 @@ def getPrograms():
             if file.endswith(".lnk"):
                 programList.append(str(os.path.join(root, file)))
 
-    with open('database/package.json', 'r', encoding='utf-8') as f:
-        JSON = json.load(f)
+    with open('database/package.json', 'r', encoding='utf-8') as file:
+        JSON = json.load(file)
         JSON["APPS"] = programList
 
-    with open('database/package.json', 'w', encoding='utf-8') as f:
-        json.dump(JSON, f, ensure_ascii=False, indent=4)
+    with open('database/package.json', 'w', encoding='utf-8') as file:
+        json.dump(JSON, file, ensure_ascii=False, indent=4)
+
+    return JSON
 
 
 if __name__ == "__main__":
@@ -181,6 +240,7 @@ if __name__ == "__main__":
 
     WIDTH = 1000
     HEIGHT = 100
+    PACKAGE = None
 
     # TODO: checkout external files
     # TODO: load database / package.json & settings.json
@@ -189,13 +249,22 @@ if __name__ == "__main__":
         with open('database/package.json', 'r', encoding='utf-8') as f:
             __JSON = json.load(f)
             if len(__JSON["APPS"]) == 0:
-                getPrograms()
+                PACKAGE = getPrograms()
+            else:
+                PACKAGE = __JSON
     except FileNotFoundError:
         with open('database/package.json', 'w'):
             pass
-        getPrograms()
+        PACKAGE = getPrograms()
 
-    main_window = MainWindow()
-    main_window.show()
+    # splash_screen = SplashScreen()
+    start = time()
+    splash = QSplashScreen(QPixmap("image/SplashScreen.png"), Qt.WindowStaysOnTopHint)
+    splash.show()
+    while time() - start < 1:
+        sleep(0.001)
+        app.processEvents()
+    MainWindow = MainWindow()
+    splash.finish(MainWindow)
 
     sys.exit(app.exec_())
